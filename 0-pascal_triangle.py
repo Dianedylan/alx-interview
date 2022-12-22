207729cf151a0224e6a5e31c722a491d91281640
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-12-22 21:31:44 +0300

    correcting errors

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle.py
-pascal_triangle(n)
+Print Pascal's Triangle
 """
 
 
 def pascal_triangle(n):
-    """
-    Returns a list of lists that rep. the Pascal’s triangle
-    """
-    if n <= 0:
-        return []
-
-    answer = []
-    i = 0
-    while i < n:
-        j = i
-        k = 0
-        level = [1]
-        while j > 1:
-            level.append(answer[-1][k] + answer[-1][k + 1])
-            j -= 1
-            k += 1
-        if i > 0:
-            level.append(1)
-        answer.append(level)
-        i += 1
-    return answer
+    """returns Pascal's triangle in a list of list"""
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([1] + [my_list[i][a] + my_list[i][a + 1]
+                              for a in range(len(my_list[i]) - 1)] + [1])
+    return my_list
